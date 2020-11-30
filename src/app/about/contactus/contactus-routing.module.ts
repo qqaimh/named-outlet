0a8a4c01b333c82366549b64801ef7c4c3642a26
xiangnan.li <xiangnan.li@transwarp.io>
2020-11-30 17:58:43 +0800

    444

--- src/app/about/contactus/contactus-routing.module.ts
+++ src/app/about/contactus/contactus-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddressComponent } from './address/address.component';
 import { ContactusComponent } from './contactus.component';
+import { FacebookComponent } from './facebook/facebook.component';
 import { PhoneComponent } from './phone/phone.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
     children:[
       { path: 'address', component: AddressComponent },
       { path: 'phone', component: PhoneComponent },
+      { path: '', component: FacebookComponent },
     ]
 
   },
--- src/app/about/contactus/contactus.module.ts
+++ src/app/about/contactus/contactus.module.ts
@@ -5,10 +5,11 @@ import { ContactusRoutingModule } from './contactus-routing.module';
 import { AddressComponent } from './address/address.component';
 import { ContactusComponent } from './contactus.component';
 import { PhoneComponent } from './phone/phone.component';
+import { FacebookComponent } from './facebook/facebook.component';
 
 
 @NgModule({
-  declarations: [ContactusComponent, AddressComponent, PhoneComponent],
+  declarations: [ContactusComponent, AddressComponent, PhoneComponent, FacebookComponent],
   imports: [
     CommonModule,
     ContactusRoutingModule
